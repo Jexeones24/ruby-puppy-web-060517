04d8739f3b1189f05ae8e5e50c0819d71e94a77d
Jexeones24 <jexeones@gmail.com>
2017-06-10 12:51:28 -0400

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Dog
+
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all<<self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each {|item| puts item.name}
+  end
+end
